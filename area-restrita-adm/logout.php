a2d2fadf229e035c59f180daccf7d949a4e3dee0
Moisés Vicente Bezerra de Souza <93298283+MoisesSouza16Tech@users.noreply.github.com>
2023-12-11 08:26:24 -0300

    Delete area-restrita-adm directory

--- area-restrita-adm/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header("Location: ../login-adm.php");
-    session_start();
-    //limpar as variáveis de sessão e destruí-las
-    unset($_SESSION['login-sessao']);
-    unset($_SESSION['senha-sessao']);
-    //destruir a sessão
-    session_destroy();
-?>
--- area-restrita-adm/validador.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    if( ($_SESSION['login-sessao'] != 'adm') || ($_SESSION['senha-sessao'] != '123') ){
-        header("Location: ../login-adm.php");
-    }
-?>
